a21c09398710e5dffd64ed7de120473a2b36367a
ShivarajMBhagoji <bshivm135@gmail.com>
2024-01-28 18:54:06 +0530

    Category Sreen Building

--- app/src/main/java/com/shivarajmb/byemoney/Pages/Categories.kt
+++ app/src/main/java/com/shivarajmb/byemoney/Pages/Categories.kt
@@ -1,39 +1,68 @@
-package com.shivarajmb.byemoney.Pages
+package com.shivarajmb.byemoney.pages
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.shivarajmb.byemoney.Components.TableRow
+import com.github.skydoves.colorpicker.compose.AlphaTile
+import com.github.skydoves.colorpicker.compose.BrightnessSlider
+import com.github.skydoves.colorpicker.compose.HsvColorPicker
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
+import com.shivarajmb.byemoney.components.TableRow
+import com.shivarajmb.byemoney.components.UnstyledTextField
+import com.shivarajmb.byemoney.ViewModels.CategoryViewModel
 import com.shivarajmb.byemoney.ui.theme.BackgroundElevated
+import com.shivarajmb.byemoney.ui.theme.ByeMoneyTheme
 import com.shivarajmb.byemoney.ui.theme.DividerColour
 import com.shivarajmb.byemoney.ui.theme.Shapes
 import com.shivarajmb.byemoney.ui.theme.TopAppBarBackground
+import com.shivarajmb.byemoney.ui.theme.Typography
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Categories(navController: NavController) {
+fun Categories(navController: NavController,viewM:CategoryViewModel= viewModel()) {
+
+    val state by viewM.uiState.collectAsState()
+
+    val colorPickerController=rememberColorPickerController()
+
     Scaffold(
         topBar = {
             MediumTopAppBar(title = { Text("Categories") },
@@ -54,32 +83,160 @@ fun Categories(navController: NavController) {
             )
         },
         content = {innerPadding ->
-            Column(modifier = Modifier.padding(innerPadding)) {
-                Column(modifier = Modifier
-                    .clickable { }
-                    .padding(16.dp)
-                    .clip(Shapes.medium)
-                    .background(BackgroundElevated)
-                    .fillMaxWidth()
+            Column(modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxHeight()
+                .background(BackgroundElevated),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
                 ) {
-                    TableRow("Groceries")
-                    Divider(modifier= Modifier.padding(start = 16.dp), thickness = 1.dp, color = DividerColour)
-                    TableRow("Gas")
-                    Divider(modifier= Modifier.padding(start = 16.dp), thickness = 1.dp, color = DividerColour)
-                    TableRow("Bills")
-                    Divider(modifier= Modifier.padding(start = 16.dp), thickness = 1.dp, color = DividerColour)
-                    TableRow("Subscription",
-                        Modifier.fillMaxWidth()){
-                        Text(
-                            text = "Delete",
-                            modifier = Modifier.background(Color.Red)
-                            )
+                    LazyColumn(modifier=Modifier.fillMaxWidth(),
+                    ){
+                        itemsIndexed(state.categoryList){index,category->
+                            TableRow(){
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(horizontal = 16.dp)
+                                ) {
+                                    Surface(
+                                        color=category.color,
+                                        shape = CircleShape,
+                                        border = BorderStroke(
+                                            width = 2.dp,
+                                            color=Color.White
+                                        ),
+                                        modifier = Modifier.size(16.dp)
+                                    ) {}
+                                    Text(
+                                        category.name,
+                                        modifier = Modifier.padding(
+                                            horizontal = 16.dp,
+                                            vertical = 10.dp
+                                        ),
+                                        style = Typography.bodyMedium
+                                    )
+                                }
+                            }
+                            if(index<state.categoryList.size-1){
+                                Divider(
+                                    modifier=Modifier.padding(start = 16.dp),
+                                    thickness = 1.dp,
+                                    color = DividerColour
+                                )
+                            }
+                        }
                     }
-                    Divider(modifier= Modifier.padding(start = 16.dp), thickness = 1.dp, color = DividerColour)
-                    TableRow("Hobbies")
                 }
 
-                Row(){
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(bottom = 16.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ){
+                    if(state.colorPickerShowing){
+                        Dialog(onDismissRequest = { viewM.hideColorPicker() }) {
+                            Surface(
+                                color=BackgroundElevated,
+                                shape = Shapes.large
+                            ){
+                            Column(
+                                modifier = Modifier.padding(30.dp)
+                            )
+                                {
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(top = 24.dp),
+                                        horizontalArrangement = Arrangement.Center,
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        AlphaTile(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .height(60.dp)
+                                                .clip(RoundedCornerShape(6.dp)),
+                                            controller = colorPickerController
+                                        )
+                                    }
+
+                                    HsvColorPicker(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(300.dp)
+                                            .padding(10.dp),
+                                        controller = colorPickerController,
+                                        onColorChanged = { envelope ->
+                                            viewM.setNewCategoryColor(envelope.color)
+                                        },
+                                    )
+                                    BrightnessSlider(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(10.dp)
+                                            .height(35.dp),
+                                        controller = colorPickerController,
+                                    )
+                                    TextButton(
+                                        onClick = viewM::hideColorPicker,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(top = 24.dp),
+                                    ) {
+                                        Text("Done")
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    Surface(
+                        onClick={ viewM.showColorPicker() },
+                        color = state.newCategoryColor,
+                        shape= CircleShape,
+                        border=BorderStroke(
+                            width = 2.dp,
+                            color = Color.White
+                        ),
+                        modifier = Modifier.size(width = 24.dp, height = 24.dp)
+                    ){}
+
+                    Surface(
+                        color = BackgroundElevated,
+                        modifier = Modifier
+                            .height(44.dp)
+                            .weight(1f)
+                            .padding(start = 16.dp),
+                        shape = Shapes.large,
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.fillMaxHeight()
+                        ) {
+                            UnstyledTextField(
+                                value = state.newCategoryName,
+                                onValueChange = viewM::setNewCategoryName,
+                                placeholder = { Text("Category name") },
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                maxLines = 1,
+                            )
+                        }
+                    }
+                    IconButton(
+                        onClick = { viewM.addNewCategory() },
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                    ) {
+                        Icon(
+                            Icons.Rounded.Send,
+                            "Adding Category"
+                        )
+                    }
 
                 }
             }
@@ -91,5 +248,7 @@ fun Categories(navController: NavController) {
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun CategoryPreview(){
-    Categories(navController = rememberNavController())
+    ByeMoneyTheme{
+        Categories(navController = rememberNavController())
+    }
 }
--- app/src/main/java/com/shivarajmb/byemoney/Pages/Expenses.kt
+++ app/src/main/java/com/shivarajmb/byemoney/Pages/Expenses.kt
@@ -1,32 +1,146 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 
-package com.shivarajmb.byemoney.Pages
+package com.shivarajmb.byemoney.pages
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.shivarajmb.byemoney.ViewModels.ExpensesViewModel
+import com.shivarajmb.byemoney.components.periodPicker
+import com.shivarajmb.byemoney.models.ExpenseDayList
+import com.shivarajmb.byemoney.models.ExpenseList
+import com.shivarajmb.byemoney.models.Recurrance
+import com.shivarajmb.byemoney.models.mockExpense
+import com.shivarajmb.byemoney.ui.theme.ByeMoneyTheme
+import com.shivarajmb.byemoney.ui.theme.LabelSecondary
 import com.shivarajmb.byemoney.ui.theme.TopAppBarBackground
+import com.shivarajmb.byemoney.ui.theme.Typography
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Expenses(navController: NavController, name: String) {
-    Scaffold(
+fun Expenses(navController: NavController,viewM:ExpensesViewModel= viewModel()){
+
+    val recurrences = listOf(
+        Recurrance.Daily,
+        Recurrance.Weekly,
+        Recurrance.Monthly,
+        Recurrance.Yearly
+    )
+
+    val state by viewM.uiState.collectAsState()
+
+    var recurranceMenuOpened by remember {
+        mutableStateOf(false)
+    }
+
+
+    Scaffold (
         topBar = {
-            MediumTopAppBar(title = { Text("Expenses") }, colors = TopAppBarDefaults.mediumTopAppBarColors(
-                containerColor = TopAppBarBackground
-            ))
+            MediumTopAppBar(
+                title = { Text("Expenses") },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = TopAppBarBackground
+                )
+            )
         },
-        content = {innerPadding ->
-            Column(modifier = Modifier.padding(innerPadding)) {
-                Text("Hello, Expenses!")
+        content = {innerPadding->
+
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Total for:",
+
+                    )
+                    periodPicker(
+                        label = state.recurrance.target,
+                        onClick = { recurranceMenuOpened=!recurranceMenuOpened },
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                    DropdownMenu(
+                        expanded =recurranceMenuOpened,
+                        onDismissRequest = { recurranceMenuOpened=false }
+                    ) {
+                        recurrences.forEach{recurrance->
+                            DropdownMenuItem(
+                                text = {Text(recurrance.target)},
+                                onClick = {
+                                    viewM.setRecurrance(recurrance)
+                                    recurranceMenuOpened=!recurranceMenuOpened
+                                }
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier=Modifier.padding(vertical = 32.dp)
+                ) {
+                    Text(
+                        text = "$",
+                        style = Typography.titleSmall,
+                        color = LabelSecondary,
+                        modifier = Modifier.padding(end = 4.dp,top=4.dp)
+                    )
+                    Text(
+                        "${state.sumTotal}",
+                        style = Typography.titleLarge
+                    )
+                }
+                ExpenseDayList(
+                    expense =mockExpense,
+                    modifier = Modifier
+                        .weight(1f)
+                        .verticalScroll(
+                            rememberScrollState()
+                        )
+                )
             }
+
         }
     )
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun ExpensesPreview(){
+    ByeMoneyTheme {
+        Expenses(navController = rememberNavController())
+    }
 }
